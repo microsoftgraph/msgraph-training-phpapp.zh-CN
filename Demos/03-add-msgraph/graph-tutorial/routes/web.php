a976b1f8b38a000eccaf45125e8b6b5635c9d600
Office GSX <OfficeGSX-GitHub@microsoft.com>
2019-03-12 11:29:26 +0100

    sync of files based on enus

---
+++ Demos/03-add-msgraph/graph-tutorial/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::get('/', 'HomeController@welcome');
+Route::get('/signin', 'AuthController@signin');
+Route::get('/callback', 'AuthController@callback');
+Route::get('/signout', 'AuthController@signout');
+Route::get('/calendar', 'CalendarController@calendar');
